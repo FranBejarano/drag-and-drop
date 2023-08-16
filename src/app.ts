bb8ff7aae3ba1fe034e726c83198a0c50c3a19da
Fran Bejarano <86791106+FranBejarano@users.noreply.github.com>
2023-08-16 17:06:23 +0100

    Render Project Items with a Class

--- src/app.ts
+++ src/app.ts
@@ -148,6 +148,27 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
     abstract renderContent(): void;
 }
 
+// ProjectItem class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+    private project: Project;
+
+    constructor(hostId: string, project: Project) {
+        super('single-project', hostId, false, project.id);
+        this.project = project;
+
+        this.configure();
+        this.renderContent();
+    }
+
+    configure() {}
+
+    renderContent() {
+        this.element.querySelector('h2')!.textContent = this.project.title;
+        this.element.querySelector('h3')!.textContent = this.project.people.toString();
+        this.element.querySelector('p')!.textContent = this.project.description;
+    }
+}
+
 // ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
     assignedProjects: Project[];
@@ -158,18 +179,6 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
 
         this.configure();
         this.renderContent();
-    }
-
-    private renderProjects() {
-        const listEl = document.getElementById
-            (`${this.type}-projects-list`
-            )! as HTMLUListElement;
-        listEl.innerHTML = '';
-        for (const prjItem of this.assignedProjects) {
-            const listItem = document.createElement('li');
-            listItem.textContent = prjItem.title;
-            listEl.appendChild(listItem);
-        }
     }
 
     configure() {
@@ -189,8 +198,17 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
         const listId = `${this.type}-projects-list`;
         this.element.querySelector('ul')!.id = listId;
         this.element.querySelector('h2')!.textContent =
-            this.type.toUpperCase() + ' PROJECTS';
-        
+            this.type.toUpperCase() + ' PROJECTS';      
+    }
+
+    private renderProjects() {
+        const listEl = document.getElementById(
+            `${this.type}-projects-list`
+            )! as HTMLUListElement;
+        listEl.innerHTML = '';
+        for (const prjItem of this.assignedProjects) {
+            new ProjectItem(this.element.querySelector('ul')!.id, prjItem);
+        }
     }
 };
 
